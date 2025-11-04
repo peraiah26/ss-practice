4f6367a971ce8430f5195380a580ef036775fb71
name <peraiahtellagorla92@gmail.com>
2025-11-04 11:53:45 +0530

    conversation

---
+++ ss-practice/conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "peraiah:: hii ganesh how are you?"
+echo "ganesh:: hello peraiah i am fine how are you man"
+echo "peraiah:: i am fine what are you doing now"
+echo "ganesh:: nothing peraiah i am lokking for job"
