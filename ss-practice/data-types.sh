b2708d9a9bdfd34db2049b326cd22f0dfb13c02c
name <peraiahtellagorla92@gmail.com>
2025-11-04 13:05:31 +0530

    hello-world

--- ss-practice/data-types.sh
+++ ss-practice/data-types.sh
@@ -6,3 +6,9 @@ number2=33
 sum=$(($number1 + $number2))
 
 echo "addition of two numbers is:$sum"
+
+
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
+echo "All leaders: ${LEADERS[@]}"
+echo "First Leader: ${LEADERS[0]}"
+echo "First Leader: ${LEADERS[10]}" 
