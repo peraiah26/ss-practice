0d68d5d68fb135adec7939c573c15dabefef2fc5
name <peraiahtellagorla92@gmail.com>
2025-11-04 12:19:08 +0530

    hello-world

---
+++ ss-practice/dec-inside-var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=ruturaj
+person2=dhoni
+
+cho "$person1:: hii $person2 how are you?"
+echo "$person2:: hello $person1 i am fine how are you man"
+echo "$person1:: i am fine what are you doing now"
+echo "$person2:: nothing $person1 i am lokking for job"
